b3879292c08073e1ca075803da70aa8e592121a6
V-mari <82954569+V-mari@users.noreply.github.com>
2021-10-13 09:28:11 -0400

    comparar dos vectores con recursividad

--- RecursividadE/src/recursividad/Ejercicios.java
+++ RecursividadE/src/recursividad/Ejercicios.java
@@ -16,37 +16,63 @@ public class Ejercicios {
 1!=1
     */
 
-    public static int factorial( int n){
+    public static int factorial(int n) {
         int factorial;
-         if ( n==1){
-             //caso base
-             factorial = n;
-         }else{
-             factorial = n*factorial(n-1);
+        if (n == 1) {
+            //caso base
+            factorial = n;
+        } else {
+            factorial = n * factorial(n - 1);
 
         }
-         return factorial;
+        return factorial;
     }
+
     /*
     Maximo comun divisor(mcd), algoritmo de Euclides
     a = 412 y b = 184
     */
-    public static int mcd(int a, int b){
-        int  mcd;
-        if ( a==b){
+    public static int mcd(int a, int b) {
+        int mcd;
+        if (a == b) {
             //caso base
             mcd = b;
 
         } else {
-            if( a > b){
+            if (a > b) {
                 a = a - b;
-            } else{
-               b = b - a;
+            } else {
+                b = b - a;
 
             }
             mcd = mcd(a, b);
         }
-        return  mcd;
+        return mcd;
     }
 
+
+    /*
+     * Metodo recursivo para comparar dos vectores de elementos por elemento
+     * vector1 = {5,6,2}
+     * vector2 = {2,9,2}
+     * diferencia=3-3+0=6
+     * */
+    private static int compararVectores(int[] vector1, int[] vector2, int indice) {
+        int sumaDiferencias;
+        if (indice == vector1.length - 1) {// caso base
+            sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]);
+
+        } else {// llamada recursiva
+
+            sumaDiferencias = Math.abs(vector1[indice] - vector2[indice]) + compararVectores(vector1, vector2, indice + 1);
+        }
+        return sumaDiferencias;
+
+    }
+
+    /*
+     * Metodo auxiliar que realiza la llamada al metodo recursivo*/
+    public static boolean compararVectores(int[] vector1, int[] vector2) {
+        return compararVectores(vector1, vector2, 0) == 0;
+    }
 }
--- RecursividadE/src/recursividad/Main.java
+++ RecursividadE/src/recursividad/Main.java
@@ -1,9 +1,15 @@
 package recursividad;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println(Ejercicios.mcd(412,184));
+
+        int[] vector1 = {5, 6, 2};
+        int [] vector2 = {2,9,2};
+        System.out.println(Ejercicios.compararVectores(vector1, vector2));
     }
 }
+
 
 
